1854ad4706e1aa9ad349a6b517d1c6e13cb822f3
宋旭华 <songxuhua@infobeats.cn>
2020-07-01 17:30:47 +0800

    add new formula idbshpass

---
+++ Formula/idbsshpass.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Idbsshpass < Formula
+  desc "随便玩玩"
+  homepage "https://github.com/songxuhua/homebrew-personDete"
+  url "https://github.com/songxuhua/homebrew-personDete/blob/master/idbsshpass-1.0.0.zip"
+  sha256 "bd725d4893311bd85acb77231eedd356c36ce9104dba362d2af1aae5f8d0b7c5"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test idbsshpass`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
